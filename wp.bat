3a3e044e6800cd3f75e5602f5ead4059a1488b10
smatkovi <38816871+smatkovi@users.noreply.github.com>
2024-10-04 13:23:19 +0200

    Update wp.bat

--- wp.bat
+++ wp.bat
@@ -1,2 +1,3 @@
 xcopy h:Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
