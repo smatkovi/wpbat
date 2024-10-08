4135b4674e13657599cdb6b2c52edc1ad65b6c95
smatkovi <38816871+smatkovi@users.noreply.github.com>
2024-10-08 15:36:49 +0200

    Update wp.bat

--- wp.bat
+++ wp.bat
@@ -1,6 +1,8 @@
 xcopy h:Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-xcopy h:\\Desktop\shutdown.lnk "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
-xcopy h:\\Desktop\reboot.lnk "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
-xcopy h:\\Desktop\logoff.lnk "%AppData%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
+cd C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch
+mkdir "User Pinned\TaskBar\"
+xcopy h:\\Desktop\shutdown.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
+xcopy h:\\Desktop\reboot.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
+xcopy h:\\Desktop\logoff.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
