63b3fcfbe2521b67d93a17fcb1caba066cb1b363
smatkovi <38816871+smatkovi@users.noreply.github.com>
2024-10-08 16:11:46 +0200

    Update wp.bat

--- wp.bat
+++ wp.bat
@@ -2,4 +2,5 @@ xcopy h:Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\The
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
 xcopy h:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+winget install -h --accept-package-agreements --accept-source-agreements 9NQ8Q8J78637
 xcopy h:\\Desktop\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
