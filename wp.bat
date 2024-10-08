230bd5d306471c90e529165d79d23eaa020c99b3
smatkovi <38816871+smatkovi@users.noreply.github.com>
2024-10-08 16:08:26 +0200

    Update wp.bat

--- wp.bat
+++ wp.bat
@@ -1,8 +1,5 @@
 xcopy h:Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-cd C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch
-mkdir "User Pinned\TaskBar\"
-xcopy h:\\Desktop\shutdown.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
-xcopy h:\\Desktop\reboot.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
-xcopy h:\\Desktop\logoff.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\" /y
+xcopy h:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+xcopy h:\\Desktop\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
