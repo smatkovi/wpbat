c54acde8024e143d809bbfdc5bf9cfcfe9e4f492
smatkovi <38816871+smatkovi@users.noreply.github.com>
2024-10-08 16:18:20 +0200

    Update wp.bat

--- wp.bat
+++ wp.bat
@@ -4,3 +4,4 @@ reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hide
 xcopy h:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
 winget install -h --accept-package-agreements --accept-source-agreements 9NQ8Q8J78637
 xcopy h:\\Desktop\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+h:\\Desktop\sdrl.ahk
