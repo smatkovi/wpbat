2292b65c9954b22ba02b7b9ff1932dd2b4111049
smatkovi <38816871+smatkovi@users.noreply.github.com>
2025-06-26 10:53:55 +0200

    Update wp.bat
    made ready for win11

--- wp.bat
+++ wp.bat
@@ -1,7 +1,8 @@
-xcopy h:Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
+xcopy i:\\Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-xcopy h:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
-winget install -h --accept-package-agreements --accept-source-agreements autohotkey.autohotkey
-xcopy h:\\Desktop\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
-h:\\Desktop\sdrl.ahk
+xcopy i:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+winget install -h --accept-package-agreements --accept-source-agreements --source msstore --id=9PLQFDG8HH9D
+xcopy i:\\Download\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+"C:\Program Files\AutoHotkey\AutoHotkey.exe" /install
+i:\\Download\sdrl.ahk
