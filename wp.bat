d3c60da7b1edc71421e0d6dc55c0252be3ae1f84
smatkovi <38816871+smatkovi@users.noreply.github.com>
2026-02-27 08:59:44 +0100

    Delete wp.bat

--- wp.bat
+++
@@ -1,8 +0,0 @@
-xcopy i:\\Download\p.jpg C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Themes\TranscodedWallpaper /y
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
-reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-xcopy i:\\Desktop\wp.bat.lnk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
-winget install -h --accept-package-agreements --accept-source-agreements --source msstore --id=9PLQFDG8HH9D
-xcopy i:\\Download\sdrl.ahk "C:\Users\smatkovich\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
-"C:\Program Files\AutoHotkey\AutoHotkey.exe" /install
-i:\\Download\sdrl.ahk
